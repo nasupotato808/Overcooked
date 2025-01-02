6b06df77fd9042957e66ed45d9b19badf6573abe
cf.le3 <cf.lee0064@gmail.com>
2025-01-02 21:24:58 +0800

    Rename "constants.py" to "strings.py"

--- todo-prototype/strings.py
+++ todo-prototype/strings.py
@@ -1,9 +1,10 @@
 ACTIVITY_NAME = "over the task goblins"
+VALUE_ERROR_POSITIVE_TASK_ID = "Task ID must be strictly positive."
 
 # Names and descriptions of bot commands.
 
 CMD_NAME_SHOW = "show"
-CMD_DESC_SHOW = "Display to-do list."
+CMD_DESC_SHOW = "Display all your tasks or the details of a specific task."
 
 CMD_NAME_TASK = "task"
 CMD_DESC_TASK = "Create, modify, or delete tasks."
@@ -22,19 +23,19 @@ CMD_DESC_TASK_DELETE = "Delete task."
 
 # Names and descriptions of bot command options.
 
-OPT_NAME_ID = "id"
-OPT_DESC_ID = "Task number. You can check this with \"\\show\"."
+OPT_NAME_TODO_ID = "todo-id"
+OPT_DESC_TODO_ID = "To-do item number. You can verify this with \"\\show\"."
 
 OPT_NAME_DESC = "description"
 OPT_DESC_DESC = "Task description."
 
 # Bot messages.
 
-MSG__SHOW_EMPTY = "Nothing to see here. Maybe add a task or two!"
+MSG_SHOW_EMPTY = "Nothing to see here. Maybe add a task or two!"
 MSG_ADD = "added to the list. Happy tasking!"
 MSG_CHECK_ON = "complete. Good job!"
 MSG_CHECK_OFF = "incomplete. Good luck!"
 MSG_EDIT = "is now"
 MSG_DELETE = "has been deleted."
 MSG_VALUE_ERR = "Sorry! I can't do anything with that task ID. Please select a task between 1 and"
-MSG_INDEX_ERR = "doesn't exist. Sorry!"
+MSG_INDEX_ERR = lambda i: f"Task {i} doesn't exist. Sorry!"
