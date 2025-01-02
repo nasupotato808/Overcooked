4727d5f6c4a86ed7b3bc70ba6c7ee94c9afc3550
cf.le3 <cf.lee0064@gmail.com>
2025-01-02 19:13:43 +0800

    Add class methods for modifying tasks in a TaskList object

--- todo-prototype/task.py
+++ todo-prototype/task.py
@@ -1,8 +1,33 @@
 from dataclasses import dataclass
 
 class TaskList:
+
     def __init__(self):
         self.tasks = []
+
+    def is_empty(self):
+        return len(self.tasks) == 0
+    
+    def add(self, task):
+        self.tasks.append()
+
+    def check(self, idx: int):
+        self.tasks[idx].complete = not self.tasks[idx].complete
+        return self.tasks[idx].complete
+    
+    def edit(self, idx: int, new_desc: str):
+        old_desc = self.tasks[idx].description
+        self.tasks[idx].description = new_desc
+        return old_desc
+    
+    def get_description(self, idx):
+        return self.tasks[idx].description
+    
+    def swap(self, idxA, idxB):
+        self.tasks[idxA], self.tasks[idxB] = self.tasks[idxB], self.tasks[idxA]
+    
+    def delete(self, idx):
+        return self.tasks.pop(idx)
 
 @dataclass
 class Task:
