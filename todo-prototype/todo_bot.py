d3b2163b964a947138321838efb501c7962ad475
cf.le3 <cf.lee0064@gmail.com>
2025-01-02 21:26:55 +0800

    Comment out commands

--- todo-prototype/todo_bot.py
+++ todo-prototype/todo_bot.py
@@ -82,61 +82,87 @@ async def task_add(ctx: SlashContext, description: str):
     required=True,
     opt_type=OptionType.INTEGER
 )
-async def task_check(ctx: SlashContext, id: int):
+# base_task = SlashCommand(name=CMD_NAME_TASK, description=CMD_DESC_TASK)
 
-    async def operation_check(id):
-        t = master.tasks[id-1]
-        t.complete = not t.complete
-        msg_complete = f"\"{t.description}\" {MSG_CHECK_ON}"
-        msg_incomplete = f"\"{t.description}\" {MSG_CHECK_OFF}"
-        await ctx.send(msg_complete if t.complete else msg_incomplete)
+# @base_task.subcommand(
+#     sub_cmd_name=CMD_NAME_TASK_ADD,
+#     sub_cmd_description=CMD_DESC_TASK_ADD
+# )
+# @slash_option(
+#     name=OPT_NAME_DESC,
+#     description=OPT_DESC_DESC,
+#     required=True,
+#     opt_type=OptionType.STRING
+# )
+# async def task_add(ctx: SlashContext, description: str):
+#     master.tasks.append(Task(description))
+#     await ctx.send(f"\"{description}\" {MSG_ADD}")
 
-    await task_id_command(ctx, operation_check, id)
+# @base_task.subcommand(
+#     sub_cmd_name=CMD_NAME_TASK_CHECK,
+#     sub_cmd_description=CMD_DESC_TASK_CHECK
+# )
+# @slash_option(
+#     name=OPT_NAME_ID,
+#     description=OPT_DESC_ID,
+#     required=True,
+#     opt_type=OptionType.INTEGER
+# )
+# async def task_check(ctx: SlashContext, id: int):
 
-@base_task.subcommand(
-    sub_cmd_name=CMD_NAME_TASK_EDIT,
-    sub_cmd_description=CMD_DESC_TASK_EDIT
-)
-@slash_option(
-    name=OPT_NAME_ID,
-    description=OPT_DESC_ID,
-    required=True,
-    opt_type=OptionType.INTEGER
-)
-@slash_option(
-    name=OPT_NAME_DESC,
-    description=OPT_DESC_DESC,
-    required=True,
-    opt_type=OptionType.STRING    
-)
-async def task_edit(ctx: SlashContext, id: int, description: str):
+#     async def operation_check(id):
+#         t = master.tasks[id-1]
+#         t.complete = not t.complete
+#         msg_complete = f"\"{t.description}\" {MSG_CHECK_ON}"
+#         msg_incomplete = f"\"{t.description}\" {MSG_CHECK_OFF}"
+#         await ctx.send(msg_complete if t.complete else msg_incomplete)
+
+#     await task_id_command(ctx, operation_check, id)
+
+# @base_task.subcommand(
+#     sub_cmd_name=CMD_NAME_TASK_EDIT,
+#     sub_cmd_description=CMD_DESC_TASK_EDIT
+# )
+# @slash_option(
+#     name=OPT_NAME_ID,
+#     description=OPT_DESC_ID,
+#     required=True,
+#     opt_type=OptionType.INTEGER
+# )
+# @slash_option(
+#     name=OPT_NAME_DESC,
+#     description=OPT_DESC_DESC,
+#     required=True,
+#     opt_type=OptionType.STRING    
+# )
+# async def task_edit(ctx: SlashContext, id: int, description: str):
     
-    async def operation_edit(id, description):
-        t = master.tasks[id-1]
-        old_desc = t.description
-        t.description = description
-        msg = f"Task {id} \"{old_desc}\" {MSG_EDIT} \"{description}\"."
-        await ctx.send(msg)
+#     async def operation_edit(id, description):
+#         t = master.tasks[id-1]
+#         old_desc = t.description
+#         t.description = description
+#         msg = f"Task {id} \"{old_desc}\" {MSG_EDIT} \"{description}\"."
+#         await ctx.send(msg)
 
-    await task_id_command(ctx, operation_edit, id, description)
+#     await task_id_command(ctx, operation_edit, id, description)
 
-@base_task.subcommand(
-    sub_cmd_name=CMD_NAME_TASK_DELETE,
-    sub_cmd_description=CMD_DESC_TASK_DELETE
-)
-@slash_option(
-    name=OPT_NAME_ID,
-    description=OPT_DESC_ID,
-    required=True,
-    opt_type=OptionType.INTEGER
-)
-async def task_delete(ctx: SlashContext, id: int):
+# @base_task.subcommand(
+#     sub_cmd_name=CMD_NAME_TASK_DELETE,
+#     sub_cmd_description=CMD_DESC_TASK_DELETE
+# )
+# @slash_option(
+#     name=OPT_NAME_ID,
+#     description=OPT_DESC_ID,
+#     required=True,
+#     opt_type=OptionType.INTEGER
+# )
+# async def task_delete(ctx: SlashContext, id: int):
 
-    async def operation_delete(id):
-        t = master.tasks.pop(id-1)
-        msg = f"Task {id} \"{t.description}\" {MSG_DELETE}"
-        await ctx.send(msg)
+#     async def operation_delete(id):
+#         t = master.tasks.pop(id-1)
+#         msg = f"Task {id} \"{t.description}\" {MSG_DELETE}"
+#         await ctx.send(msg)
 
-    await task_id_command(ctx, operation_delete, id)
+#     await task_id_command(ctx, operation_delete, id)
 
 client.start()
