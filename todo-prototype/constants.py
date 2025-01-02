6a71a0bee1131b6dc78a825fe64b9f385802da12
cf.le3 <cf.lee0064@gmail.com>
2025-01-02 19:21:23 +0800

    Create new file "constants.py"
    Contains definitions for string literals used by the bot.

---
+++ todo-prototype/constants.py
@@ -0,0 +1,40 @@
+ACTIVITY_NAME = "over the task goblins"
+
+# Names and descriptions of bot commands.
+
+CMD_NAME_SHOW = "show"
+CMD_DESC_SHOW = "Display to-do list."
+
+CMD_NAME_TASK = "task"
+CMD_DESC_TASK = "Create, modify, or delete tasks."
+
+CMD_NAME_TASK_ADD = "add"
+CMD_DESC_TASK_ADD = "Create a new task."
+
+CMD_NAME_TASK_CHECK = "check"
+CMD_DESC_TASK_CHECK = "Toggle task completion status."
+
+CMD_NAME_TASK_EDIT = "edit"
+CMD_DESC_TASK_EDIT = "Edit task description."
+
+CMD_NAME_TASK_DELETE = "delete"
+CMD_DESC_TASK_DELETE = "Delete task."
+
+# Names and descriptions of bot command options.
+
+OPT_NAME_ID = "id"
+OPT_DESC_ID = "Task number. You can check this with \"\\show\"."
+
+OPT_NAME_DESC = "description"
+OPT_DESC_DESC = "Task description."
+
+# Bot messages.
+
+MSG__SHOW_EMPTY = "Nothing to see here. Maybe add a task or two!"
+MSG_ADD = "added to the list. Happy tasking!"
+MSG_CHECK_ON = "complete. Good job!"
+MSG_CHECK_OFF = "incomplete. Good luck!"
+MSG_EDIT = "is now"
+MSG_DELETE = "has been deleted."
+MSG_VALUE_ERR = "Sorry! I can't do anything with that task ID. Please select a task between 1 and"
+MSG_INDEX_ERR = "doesn't exist. Sorry!"
